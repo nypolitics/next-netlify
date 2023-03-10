1e5c382685c1f4b0085409c92e9a92e7275bfeb1
nypolitics <ed@upstatenypolitics.com>
2023-03-09 23:02:41 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import { Navbar } from '@components/Navbar';
+// import { Navbar } from '@components/Navbar';
 
 export default function Home() {
   return (
@@ -10,7 +10,7 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NavBar />
+      //<NavBar />
       <main>
         <Header title="The Start of Next Development!" />
         <p className="description">
