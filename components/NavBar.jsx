cf20146ce88e4e0367dea57f52ec245f53e642c2
nypolitics <ed@upstatenypolitics.com>
2023-03-09 23:26:43 -0500

    commit

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,7 +2,7 @@
 
 export const Navbar = () => {
   return (
-    <div>
+    <>
       <nav className='flex items-center flex-wrap bg-green-300 p-3 '></nav>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,17 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-// import { Navbar } from '@components/Navbar';
+import { Navbar } from '../components/Navbar';
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Create Next App</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-      //<NavBar />
-      <main>
-        <Header title="The Start of Next Development!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+      <Navbar />
+      <div>Hello World</div>
     </div>
-  )
+  );
 }
